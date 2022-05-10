e6dfe4d6cbc58bbf21050453665e39e2097725ba
Judit Csokay <juditcsokay@gmail.com>
2022-05-10 17:22:15 +0200

    Configure routing for the header links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
@@ -11,6 +12,10 @@ import { BookListComponent } from './books/book-list/book-list.component';
 import { BookDetailComponent } from './books/book-detail/book-detail.component';
 import { BookItemComponent } from './books/book-list/book-item/book-item.component';
 
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'olvasmanyaim', component: BookListComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +25,13 @@ import { BookItemComponent } from './books/book-list/book-item/book-item.compone
     BookDetailComponent,
     BookItemComponent,
   ],
-  imports: [FormsModule, BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
